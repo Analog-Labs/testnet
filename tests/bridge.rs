ea9ceee73ed37786b1f628e3d6d407a45048af67
Alexander Gryaznov <hi@agryaznov.com>
2025-02-20 20:38:10 +0200

    + bridge pot balance assertions

--- tests/bridge.rs
+++ tests/bridge.rs
@@ -4,7 +4,6 @@ use metadata::runtime_types::pallet_assets_bridge::pallet::Call as BridgeCall;
 use metadata::runtime_types::pallet_assets_bridge::types::NetworkData;
 
 use sp_core::crypto::AccountId32 as Address32;
-use tc_cli::Tc;
 use tracing_subscriber::filter::EnvFilter;
 
 mod common;
@@ -14,15 +13,13 @@ use subxt::utils::Static;
 use subxt_signer::sr25519::dev;
 use tc_subxt::metadata;
 
-use time_primitives::{Gateway, NetworkId};
+use time_primitives::NetworkId;
 
-const TC: NetworkId = 1000;
 const EVM: NetworkId = 2;
 
 const CONFIG: &str = "local-e2e-bridge.yaml";
 const PROFILE: &str = "bridge";
 
-const GATEWAY: Address20 = address!("0x49877F1e26d523e716d941a424af46B86EcaF09E");
 const ERC20: Address20 = address!("0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0");
 const BENEFICIARY: Address20 = address!("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
 const AMOUNT_OUT: u128 = 15_000_000_000_000;
@@ -48,6 +45,7 @@ async fn to_erc20() {
 		nonce: 0,
 		dest: Static(Address32::new(ERC20.into_word().0)),
 	};
+
 	//	4. Register network EVM as teleportation target at pallet_bridge
 	let api = &env.tc.runtime().client;
 	let call = metadata::RuntimeCall::Bridge(BridgeCall::register_network {
@@ -76,8 +74,19 @@ async fn to_erc20() {
 		.expect("BridgeStatusChanged event missed");
 	tracing::info!(target: "bridge_test", "BridgeStatusChanged event found: {reg_event:?}");
 
-	let tc_bal_before = &env.tc.runtime().balance(&from_acc).await.expect("cannot query sender balance");
+	let tc_bal_before =
+		&env.tc.runtime().balance(&from_acc).await.expect("cannot query sender balance");
 	tracing::info!(target: "bridge_test", "Sender bal before: {tc_bal_before}");
+	let query = metadata::constants().bridge().bridge_pot();
+	let bridge_pot = api.constants().at(&query).expect("cannot query bridge pot address");
+	let bridge_bal_before = &env
+		.tc
+		.runtime()
+		.balance(&bridge_pot)
+		.await
+		.expect("cannot query bridge balance");
+	// TODO why is it larger than ED at this point??
+	tracing::info!(target: "bridge_test", "Bridge bal before: {bridge_bal_before}");
 
 	//	5. Dispatch extrinsic for teleporting TC->ERC20
 	let tx = metadata::tx().bridge().teleport_keep_alive(
@@ -101,15 +110,28 @@ async fn to_erc20() {
 		.expect("Teleported event missed");
 	tracing::info!(target: "bridge_test", "Teleported event found: {tel_event:?}");
 
-	let tc_bal_after = &env.tc.runtime().balance(&from_acc).await.expect("cannot query sender balance");
+	// 7. Check source balance(s)
+	let tc_bal_after =
+		&env.tc.runtime().balance(&from_acc).await.expect("cannot query sender balance");
 	tracing::info!(target: "bridge_test", "Sender bal after: {tc_bal_after}");
+	let bridge_bal_after = &env
+		.tc
+		.runtime()
+		.balance(&bridge_pot)
+		.await
+		.expect("cannot query bridge balance");
+	tracing::info!(target: "bridge_test", "Bridge bal after: {bridge_bal_after}");
 
-	// Sender paid teleported ampunt plus some fees
+	// Sender paid teleported amount plus some fees
 	assert!(tc_bal_after.saturating_add(AMOUNT_OUT) < *tc_bal_before);
+	// Bridge Pot should get the exact teleported amount
+	assert_eq!(bridge_bal_after.saturating_sub(*bridge_bal_before), AMOUNT_OUT);
 
+	// 7. Wait for task to complete (or batch to get tx_hash)
+
+	// 8. Check destination balance
 
 	/*
-	5. Wait for task to complete (or batch to get tx_hash) & check the resulting balance(s)
 	ERC20->TC
 	6. call estimateTeleport
 	7. send teleport tx ERC20->TC
